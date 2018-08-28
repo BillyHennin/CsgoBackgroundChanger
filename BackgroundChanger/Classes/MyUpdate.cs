8cd78baa68e0174f3c3479759195aed66911190d
Billy Hennin <billy.hennin@gmail.com>
2018-08-28 22:08:03 +0200

    Fixed async, prepare app for future updates, add icon, prepare setting page

---
+++ BackgroundChanger/Classes/MyUpdate.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using BackgroundChanger.Pages;
+using MahApps.Metro.Controls.Dialogs;
+using Newtonsoft.Json.Linq;
+
+namespace BackgroundChanger.Classes
+{
+    public static class MyUpdate
+    {
+        private const string Url = "https://billyhennin.github.io/CsgoBackgroundChanger/";
+
+        public static async Task<object> CheckUpdate(MainWindow window)
+        {
+            var request = (HttpWebRequest) WebRequest.Create(Url);
+            try
+            {
+                var response = request.GetResponse();
+                using (var stream = response.GetResponseStream())
+                {
+                    var result =
+                        JObject.Parse(new StreamReader(stream ?? throw new InvalidOperationException(), Encoding.UTF8)
+                            .ReadToEnd());
+                    if (string.IsNullOrEmpty(result["Version"].ToString()))
+                    {
+                        throw new Exception();
+                    }
+
+                    if (Assembly.GetExecutingAssembly().GetName().Version
+                            .CompareTo(new Version(result["Version"].ToString())) >= 0)
+                    {
+                        return null;
+                    }
+
+                    window.UpdateFlyOut.IsOpen = true;
+                    window.LbTitleUpdate.Content = string.IsNullOrEmpty(result["Title"].ToString())
+                        ? "A new update is available"
+                        : result["Title"].ToString();
+                    window.LbDescUpdate.Content = string.IsNullOrEmpty(result["Description"].ToString())
+                        ? "Check the website for more infos."
+                        : result["Description"].ToString();
+                    return null;
+                }
+            }
+            catch
+            {
+                await window.ShowMessageAsync("Error", "An error has occured while checking for an update.");
+                return null;
+            }
+        }
+    }
+}
--- BackgroundChanger/Pages/MainWindow.xaml.cs
+++ BackgroundChanger/Pages/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Media;
@@ -16,15 +17,24 @@ namespace BackgroundChanger.Pages
         public MainWindow()
         {
             InitializeComponent();
-            MyRegedit.CheckRegedit();
-            FillList();
+            InitWindow();
         }
 
-        public async void FillList()
+        public async void InitWindow()
         {
-            //TODO : Afficher ProgressAsync
-            const string strMessage = "Loading webm.... ";
-            var controller = await this.ShowProgressAsync("Please wait", strMessage);
+            MyRegedit.CheckRegedit();
+            if(await MyUpdate.CheckUpdate(this) == null) await FillList();
+        }
+
+        private void SetVisibility(bool isVisible)
+            => WebmPlayer.Visibility = LbTitle.Visibility = LbInfos.Visibility =
+                SelectBtn.Visibility = isVisible ? Visibility.Visible : Visibility.Hidden;
+
+        public async Task FillList()
+        {
+            const string msg = "Loading webm.... ";
+            var controller = await this.ShowProgressAsync("Please wait", msg);
+            controller.SetIndeterminate();
             WebmList.Items.Clear();
             var allWebm = MyFolders.GetAllWebm();
             if (allWebm.Length == 0)
@@ -34,9 +44,11 @@ namespace BackgroundChanger.Pages
                 return;
             }
 
+            controller.Minimum = 0;
+            controller.Maximum = allWebm.Length;
+
             var progress = 100 / allWebm.Length / 100;
-            var i = 0.0;
-            var y = 0;
+            var y = 1;
             foreach (var file in allWebm)
             {
                 WebmList.Items.Add(new MediaElement
@@ -47,11 +59,11 @@ namespace BackgroundChanger.Pages
                     Stretch = Stretch.Fill,
                     Margin = new Thickness(0, 5, 0, 5)
                 });
-                controller.SetProgress(i += progress);
-                controller.SetMessage(strMessage + y++ + "/" + allWebm.Length);
+                controller.SetProgress(y += progress);
+                controller.SetMessage($"{msg}{y++}/{allWebm.Length}");
+                await Task.Delay(500);
             }
-
-            await controller.CloseAsync();
+            await controller.CloseAsync().ConfigureAwait(false);
         }
 
         private void ListView_SelectionChanged(object sender, SelectionChangedEventArgs e)
@@ -63,25 +75,25 @@ namespace BackgroundChanger.Pages
             else
             {
                 WebmPlayer.Source = ((MediaElement) WebmList.SelectedItem).Source;
-                var uri = WebmPlayer.Source.ToString();
-                LbTitle.Content = MyFolders.RemoveUri(uri);
-                //TODO : Get file info
-                //LbInfos.Content = (new System.IO.FileInfo(uri.Replace("file:///", string.Empty))).Length;
                 SetVisibility(true);
             }
         }
-
-        private async void SelectBtn_Click(object sender, RoutedEventArgs e)
+        private void WebmPlayer_MediaOpened(object sender, RoutedEventArgs e)
+        {
+            LbTitle.Content = MyFolders.RemoveUri(WebmPlayer.Source.ToString());
+            LbInfos.Content = WebmPlayer.NaturalVideoWidth + " x " + WebmPlayer.NaturalVideoHeight
+                              + " (" + WebmPlayer.NaturalDuration.TimeSpan.ToString(@"mm\:ss") + ")";
+        }
+        private void SelectBtn_Click(object sender, RoutedEventArgs e)
         {
             MyFolders.UpdateBackground(this, MyFolders.RemoveUri(WebmPlayer.Source.ToString()));
-            //TODO : Update CSGO files
-            await this.ShowMessageAsync("Background changed", "Enjoy your new background !");
+            this.ShowMessageAsync("Background changed", "Enjoy your new background !");
         }
+        private async void BtnRefresh_Click(object sender, RoutedEventArgs e) => await FillList();
 
-        private void BtnRefresh_Click(object sender, RoutedEventArgs e) => FillList();
+        private void BtnSettings_Click(object sender, RoutedEventArgs e)
+        {
 
-        private void SetVisibility(bool isVisible)
-            => WebmPlayer.Visibility = LbTitle.Visibility = LbInfos.Visibility =
-                SelectBtn.Visibility = isVisible ? Visibility.Visible : Visibility.Hidden;
+        }
     }
 }
